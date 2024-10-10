1ffe8e77dbeabcefe736a7df2579f73f45a92a12
hcy08 <sweet17strawberry@gmail.com>
2024-10-10 19:20:39 +0900

    Editor.jsx update

---
+++ src/Editor.jsx
@@ -0,0 +1,31 @@
+import React, { memo, useEffect, useRef } from "react";
+import EditorJS from "@editorjs/editorjs";
+import { EDITOR_JS_TOOLS } from "./tools";
+
+const Editor = ({ data, onChange, editorblock }) => {
+  const ref = useRef();
+  useEffect(() => {
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: editorblock,
+
+        tools: EDITOR_JS_TOOLS,
+        data: data,
+        async onChange(api, event) {
+          const data = await api.saver.save();
+          onChange(data);
+        },
+      });
+      ref.current = editor;
+    }
+
+    return () => {
+      if (ref.current && ref.current.destroy) {
+        ref.current.destroy();
+      }
+    };
+  }, []);
+  return <div id={editorblock} />;
+};
+
+export default memo(Editor);
---
+++ src/tools.jsx
@@ -0,0 +1,16 @@
+import Paragraph from "@editorjs/paragraph";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Delimiter from "@editorjs/delimiter";
+import CheckList from "@editorjs/checklist";
+
+export const EDITOR_JS_TOOLS = {
+  paragraph: {
+    class: Paragraph,
+    inlineToolbar: true,
+  },
+  header: Header,
+  checkList: CheckList,
+  list: List,
+  delimiter: Delimiter,
+};
